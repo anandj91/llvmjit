1578ae9359785a933e7e254b9b4781242e16a9ce
Anand J <anandj@cs.toronto.edu>
2021-06-10 21:50:01 -0400

    Loop implementation

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/SourceMgr.h"
+#include "llvm/IR/Verifier.h"
 
 #include "jit.h"
 
@@ -15,6 +16,7 @@
 #include <memory>
 #include <string>
 
+using namespace std;
 using namespace llvm;
 using namespace llvm::orc;
 
@@ -24,22 +26,37 @@ std::unique_ptr<Module> buildprog(LLVMContext& ctx)
 
     Module* m = llmod.get();
 
-    Function* add1_fn = Function::Create(
-        FunctionType::get(Type::getInt32Ty(ctx), { Type::getInt32Ty(ctx) }, false),
-        Function::ExternalLinkage, "add1", m);
+    auto loop_fn = Function::Create(
+        FunctionType::get(Type::getInt32Ty(ctx), { Type::getInt32Ty(ctx), Type::getInt32Ty(ctx) }, false),
+        Function::ExternalLinkage, "loop", m);
+    auto start = loop_fn->getArg(0);
+    start->setName("start");
+    auto end = loop_fn->getArg(1);
+    end->setName("end");
 
-    BasicBlock* bb = BasicBlock::Create(ctx, "EntryBlock", add1_fn);
+    auto entry_bb = BasicBlock::Create(ctx, "entry", loop_fn);
+    auto body_bb = BasicBlock::Create(ctx, "body", loop_fn);
+    auto exit_bb = BasicBlock::Create(ctx, "exit", loop_fn);
+    IRBuilder<> builder(entry_bb);
+    builder.CreateBr(body_bb);
+    builder.SetInsertPoint(body_bb);
 
-    IRBuilder<> builder(bb);
+    auto i = builder.CreatePHI(Type::getInt32Ty(ctx), 2, "i");
+    auto sum = builder.CreatePHI(Type::getInt32Ty(ctx), 2, "sum");
 
-    Value* one = builder.getInt32(1);
+    auto next_i = builder.CreateAdd(i, builder.getInt32(1), "next_i");
+    i->addIncoming(start, entry_bb);
+    i->addIncoming(next_i, body_bb);
 
-    Argument* argx = add1_fn->getArg(0);
-    argx->setName("x");
+    auto sum_expr = builder.CreateAdd(i, sum, "new_sum");
+    sum->addIncoming(builder.getInt32(0), entry_bb);
+    sum->addIncoming(sum_expr, body_bb);
 
-    Value* add = builder.CreateAdd(one, argx);
+    auto loop_cond = builder.CreateICmpSLT(i, end, "cond");
+    builder.CreateCondBr(loop_cond, body_bb, exit_bb);
 
-    builder.CreateRet(add);
+    builder.SetInsertPoint(exit_bb);
+    builder.CreateRet(sum);
 
     return llmod;
 }
@@ -66,7 +83,7 @@ std::unique_ptr<Module> buildsrc(LLVMContext& Context)
     }
 }
 
-int main()
+int main(int argc, char** argv)
 {
     InitializeNativeTarget();
     InitializeNativeTargetAsmPrinter();
@@ -78,12 +95,15 @@ int main()
     //auto llmod = std::move(buildsrc(ctx));
     if (!llmod) return -1;
 
+    raw_fd_ostream r(fileno(stdout), false);
+    if (!verifyModule(*llmod, &r)) { r << *llmod; }
+
     cantFail(jit->addModule(std::move(llmod)));
 
-    JITEvaluatedSymbol sym = cantFail(jit->lookup("add1"));
+    JITEvaluatedSymbol sym = cantFail(jit->lookup("loop"));
 
-    auto* add1 = (int (*)(int))(intptr_t)sym.getAddress();
-    std::cout << "Result: " << add1(10) << std::endl;
+    auto* loop = (int (*)(int, int))(intptr_t)sym.getAddress();
+    std::cout << "Result: " << loop(atoi(argv[1]), atoi(argv[2])) << std::endl;
 
     return 0;
 }
